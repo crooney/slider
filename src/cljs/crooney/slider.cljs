38655a1351ade65be5abeccbb91325455751ea9b
Chris Rooney <christopher.j.rooney@gmail.com>
2013-07-23 19:28:47 -0430

    minor refactoring, cleanup and docstrings

--- src/cljs/crooney/slider.cljs
+++ src/cljs/crooney/slider.cljs
@@ -1,37 +1,46 @@
 (ns crooney.slider
-  (:require [clojure.browser.repl :as repl]
-            [enfocus.core :as ef]
+  (:require [enfocus.core :as ef]
             [enfocus.events :as events]
             [enfocus.effects :as effects]
-            [cljs.core.async :as async :refer [timeout <! >! chan close!]])
-  (:require-macros [cljs.core.async.macros :as am :refer [go alt!]]
+            [cljs.core.async :as async :refer [timeout <! >! chan]])
+  (:require-macros [cljs.core.async.macros :as am :refer [go]]
                    [enfocus.macros :as em]))
 
-(repl/connect "http://localhost:9000/repl")
+(defn class-selectors [x]
+  "Extract all ids that match selector x and make them id selectors: e.g. #foo."
+  (map #(str "#" %)
+       (:p (ef/from js/document :p [x] (ef/get-prop :id)))))
 
 (defn default-transition [from to t]
+  "Simple simultaneous fade in/out lasting t milliseconds."
   (ef/at js/document
          [from] (effects/fade-out t)
          [to]   (effects/fade-in  t)))
 
 (defn start-slider
+  "Start an indefinite slideshow of children of div 'id' that have the 'pane'
+  class. If div 'id' also has children of the button class the first two act
+  as backward and forward buttons in that order. There is a period of 'pause'
+  milliseconds between slides and each transition lasts 'trans-time'.
+  'trans-time' is part of the 'pause', and not additional. 'pane's should have
+  absolute position and fill 100% of the parent div 'id'. 'transition' should
+  be a function taking from and to selectors followed by 'trans-time'.
+  'default-transition' provides a pleasing fade in/out."
   [id pause trans-time transition]
-  (let [sels (fn [x] (map #(str "#" %)
-                          (:p (ef/from js/document :p [x] (ef/get-prop :id)))))
-        ps (sels ".pane")
-        bs (sels ".button")
+  (let [ps (class-selectors ".pane")
+        bs (class-selectors ".button")
         c (chan)
         btn-click (fn [x] (events/listen :click #(go (>! c x))))]
     (when (> (count bs) 1)
       (ef/at js/document
              [(first bs)] (btn-click (dec (count ps)))
              [(second bs)] (btn-click 1)))
-    (go (while true (<! (timeout pause)) (>! c 1)))
+    (when (pos? pause)
+      (go (while true (<! (timeout pause)) (>! c 1))))
     (go (loop [is (cycle ps)]
           (let [js (drop (<! c) is)]
             (transition (first is) (first js) trans-time)
             (recur js))))))
-  ;;[id pause trans-time] (start-slider id pause trans-time default-transition))
 
 (set! (.-onload js/window) #(start-slider "example" 4000 500 default-transition))
 
