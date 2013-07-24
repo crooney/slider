29bdf8d3f7625e1ae00095e1f072275858240664
Chris Rooney <christopher.j.rooney@gmail.com>
2013-07-24 14:50:02 -0430

    clean up for push to github.

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # slider
 
-A Clojure library designed to ... well, that part is up to you.
+A simple Clojurescript library to implement interactive slideshows. Slides may be images, divs or anything else.
+
+A page may have several slideshows, and ids may be repeated in different slideshows. The delay between slides and the time of transmission may be specified in HTML or in code. Different transitions may be coded.
+
 
 ## Usage
 
-FIXME
+Slideshows should be a relativelt positioned div with class `slider`. It should have as children an arbitrary number of absolutely positioned elements of class `pane`. If backward and forward buttons are desired, the should have the class `button`. The first `button` is back and the second forward.
+
+To start automatically, slideshows need the slider div to have `data-pause` and `data-trans-time` attributes, which specify the delay and transition times in milliseconds. The only script then necessary for any amount of slideshows is `<script>window.onload=crooney.slider.start_all</script>`.
+
+To start individually through javascript the call is `crooney.slider.start(`*id,delay,transition-time*`)`.
 
 ## License
 
-Copyright © 2013 FIXME
+Copyright © 2013 Christopher Rooney
 
 Distributed under the Eclipse Public License, the same as Clojure.
--- project.clj
+++ project.clj
@@ -1,20 +1,17 @@
-(defproject crooney/slider "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject crooney/slider "0.1.0"
+  :description "Clojurescript slideshow library"
+  :url "http://github.com/crooney/slider"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/clj"]
   ;; for core.async
-  ;;:repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :repositories {"sonatype-staging"
-                 "https://oss.sonatype.org/content/groups/staging/"}
+  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
+  ;;:repositories {"sonatype-staging" "https://oss.sonatype.org/content/groups/staging/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [enfocus "2.0.0-SNAPSHOT"]
-                 [org.clojure/core.async "0.1.0-SNAPSHOT"]
-                 [ring/ring-jetty-adapter "1.2.0"]]
-  :plugins [[lein-cljsbuild "0.3.2"]
-            [lein-ring "0.8.6"]]
+                 [org.clojure/core.async "0.1.0-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "0.3.2"]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds
               {:dev
@@ -27,5 +24,4 @@
                 :compiler {:output-to "resources/public/js/slider.js"
                            :optimizations :advanced
                            :pretty-print false}
-                :jar true}}}
-  :ring {:handler crooney.server/app})
+                :jar true}}})
